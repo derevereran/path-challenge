b083de6de543914f2db3cf9bf3254c89f1c7fe23
devran <atesdevran@gmail.com>
2021-09-06 12:25:43 +0300

    Task/Path/init

---
+++ PathApp/PathApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PathApp
+//
+//  Created by Devran Ates on 6.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
